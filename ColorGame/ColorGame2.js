7febfde94e8e4c797be3e089359bdf28686de315
devanshsingh201001 <56143122+devanshsingh201001@users.noreply.github.com>
2020-05-17 08:08:20 +0530

    Adding Game

---
+++ ColorGame/ColorGame2.js
@@ -0,0 +1,143 @@
+var numSquares = 6;
+var pickedColor;
+var colors = [];
+var message = document.querySelector("#message");
+var squares = document.querySelectorAll(".square");
+var h1 = document.querySelector("h1");
+var reset1 = document.querySelector("#Reset1");
+var easy = document.querySelector("#Easy");
+var hard = document.querySelector("#Hard");
+
+// var reset = document.querySelector("#NewColours");
+var dispPickedColor = document.querySelector("#PickedColor");
+
+// Generating Square
+function sqGen(numSquares)
+{
+     	squareGenerator(numSquares);
+     	pickedColor = colors[Math.floor(Math.random() * numSquares)];
+  		dispPickedColor.textContent = pickedColor;
+
+  		for(var j=0;j<squares.length;j++)
+  		{
+  			if(j<numSquares)
+  			{
+  				squares[j].style.background = colors[j];
+  				squares[j].display = "block";
+  			}
+  			else
+  			{
+  				squares[j].display = "none";
+  			}
+  		}
+  		for (var i=0;i<numSquares;i++)
+  		{
+     	squares[i].addEventListener("click", function(){
+		if(this.style.background === pickedColor)
+		{
+			message.textContent = "Correct";
+			reset1.textContent= "Play Again";
+			for(var j=0 ; j< numSquares ; j++)
+			{
+				squares[j].style.background = pickedColor;
+				h1.style.background = pickedColor;
+			}
+		}
+		else
+		{
+			message.textContent = "Try Again";
+			this.style.background = "#232323";
+		}
+	})
+  	}	
+}
+
+function squareGenerator(numSquares){
+	colors=[]
+	for ( var j =0;j<numSquares;j++)
+	{
+		colors[j]=colorGen();
+	}
+}
+// for(var i =0 ; i < numSquares ; i++)	
+// {squares[i].addEventListener("click", function(){
+// 		if(this.style.background === pickedColor)
+// 		{
+// 			message.textContent = "Correct";
+// 			for(var j=0 ; j< numSquares ; j++)
+// 			{
+// 				squares[j].style.background = pickedColor;
+// 				h1.style.background = pickedColor;
+// 			}
+// 		}
+// 		else
+// 		{
+// 			message.textContent = "Try Again";
+// 			this.style.background = "#232323";
+// 		}
+	
+// })
+// }
+
+
+
+
+// Generating Random Colors
+function colorGen()
+{
+	// Generating red color
+	var r = Math.floor(Math.random()*256);
+	// Generating green color
+	var g = Math.floor(Math.random()*256);
+	// Generating blue color
+	var b = Math.floor(Math.random()*256);
+    var colortemp = "rgb(" + r + ", " + g + ", " + b + ")";
+    return colortemp;
+}
+
+reset1.addEventListener("click",function(){
+	reset(numSquares);
+});
+
+
+function reset(numSquares)
+{
+	sqGen(numSquares);
+	h1.style.background = "steelblue";
+	message.textContent = "";
+	reset1.textContent = "New Colours";
+	for (var j=0;j<6;j++)
+	{
+		if(j<numSquares)
+		{
+			squares[j].style.display = "block"
+		}
+		else
+		{
+			squares[j].style.display = "none";
+		}
+	}
+
+}
+
+easy.addEventListener("click",function(){
+	numSquares=3;
+	reset(3);
+	easy.classList.add("selected");
+	hard.classList.remove("selected");
+
+})
+
+hard.addEventListener("click",function(){
+	numSquares=6;
+	reset(6);
+	hard.classList.add("selected");
+	easy.classList.remove("selected");
+
+})
+sqGen(6);
+
+function dispSquares()
+{
+
+}
